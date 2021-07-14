768f9e6754c762ff4a7b428c13bd515667280757
nikhilsn01 <nikhilsn360@gmail.com>
2021-07-14 23:01:31 +0530

    worked on orm to create tables

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
+
 
 def home(request):
-    return HttpResponse('<h1>Home page blog</h1>')
+    context = {
+        'posts':Post.objects.all(),
+        'title':"Blog-Home"
+    }
+    return render(request,'blog/home.html',context)
 
 def about(request):
-    return HttpResponse('<h1>About page blog from nikhil S N</h1>')
+    return render(request,'blog/about.html')
+
