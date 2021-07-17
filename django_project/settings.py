7e31798c5506d96ae3ea9c2212d49dc8ce59bcc7
nikhilsn01 <nikhilsn360@gmail.com>
2021-07-17 09:39:50 +0530

    added profilepicture for user

--- django_project/settings.py
+++ django_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- django_project/urls.py
+++ django_project/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 from users import views as user_views
 
 urlpatterns = [
@@ -25,5 +27,5 @@ urlpatterns = [
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'), name = 'logout'),
     path('',include('blog.urls')),
     path('profile/',user_views.profile,name='user-profile'),
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
